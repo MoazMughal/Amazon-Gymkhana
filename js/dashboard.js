43fd2299092c278cee62d123ba8b8de46a21b88f
Moaz Javed <Moazmughal786@gmail.com>
2025-11-02 16:43:45 +0500

    Code Updated

--- js/dashboard.js
+++
@@ -1,5 +0,0 @@
-// Additional dashboard behaviors (auth.js binds form submit)
-document.addEventListener('DOMContentLoaded', function() {
-    // TODO: load listings for the current user if Firestore configured
-});
-
